8b626f74d2fe4c5bf928b75fab49403b1c1dd73c
Ailton RibeiroDev <ribeiroailtonalves@gmail.com>
2022-07-17 10:44:56 -0300

    ProductDetails route

--- frontweb/src/pages/Home/ProductDetails/index.tsx
+++ frontweb/src/pages/Home/ProductDetails/index.tsx
@@ -1,7 +1,45 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/Seta.svg';
+import ProductPrice from 'components/ProductPrice/ProductPrice';
+import './styles.css'
+
 const ProductDetails = () => {
   return (
-    <h1>Página de detalhes</h1>
-  )
-}
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt="Nome do Produto"
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do produto</h1>
+              <ProductPrice price={2345.89} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do produto</h2>
+              <p>
+                Seja um mestre em multitarefas com a capacidade para exibir
+                quatro aplicativos simultâneos na tela. A tela está ficando
+                abarrotada? Crie áreas de trabalho virtuais para obter mais
+                espaço e trabalhar com os itens que você deseja. Além disso,
+                todas as notificações e principais configurações são reunidas em
+                uma única tela de fácil acesso.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
